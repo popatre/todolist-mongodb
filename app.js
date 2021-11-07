6f3a29958485c29570de1489010336d0b0bb33d4
popatre <76402245+popatre@users.noreply.github.com>
2021-11-07 15:50:13 +0000

    fixes db

--- app.js
+++ app.js
@@ -8,10 +8,8 @@ app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-const password = process.env.password;
-
 mongoose.connect(
-    "mongodb+srv://jmcguire:`${password}`@cluster0.ytwqw.mongodb.net/todolistdb"
+    "mongodb+srv://jmcguire:qwerty111@cluster0.ytwqw.mongodb.net/todolistdb"
 );
 
 const itemsSchema = {
